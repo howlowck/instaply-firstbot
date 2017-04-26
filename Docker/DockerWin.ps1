18c049e3c094363b278ee4e2b562122b3927662e
Kevin Remde <kevin@remde.net>
2017-04-26 10:33:22 -0500

    Minor cleanups

--- Docker/DockerWin.ps1
+++ Docker/DockerWin.ps1
@@ -1,5 +1,5 @@
 # CD
-cd C:\Code\instaply-firstbot
+cd C:\Code\instaply-firstbot\Docker
 
 docker build -t app-image .
 
@@ -7,7 +7,7 @@ docker images
 
 # Run it
 # docker run -d -p 8000:8000 --name my-app-cntr my-app-image ping -t localhost  
-docker run -d -p 8080:80 --name app-cntr app-image 
+docker run -d -it -p 8080:80 --name app-cntr app-image /bin/bash
 
 
 # Check the containers
@@ -35,33 +35,20 @@ docker rmi app-image
 docker rmi karcontainers-microsoft.azurecr.io/samples/app-image
 docker images
 
-docker run -d -p 8080:80 --name app-cntr app-image 
+# Try running it locally, requiring it to be pulled from the registry
 docker run -d -p 8080:80 --name app-cntr karcontainers-microsoft.azurecr.io/samples/app-image  
+docker exec -it app-cntr /bin/bash
 
-docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" my-site
 
-
-# Clean up
-docker stop my-site
-docker rm my-site
-docker ps -a
-
-docker rmi nanoserver
-docker rmi karcontainers-microsoft.azurecr.io/samples/iis-site
-
-docker images
-
-docker rmi karcontainers-microsoft.azurecr.io/samples/nanoserver
 
 # Clean up
-docker logout karcontainers-microsoft.azurecr.io/samples/nanoserver
-
-docker rm demo2
+# Delete local tags and images
+docker rmi app-image
+docker rmi karcontainers-microsoft.azurecr.io/samples/app-image
 docker ps -a
-
-docker rmi iis-site
-docker rmi nanoserver
-docker rmi microsoft/nanoserver
-docker rmi karcontainers-microsoft.azurecr.io/samples/nanoserver
 docker images
+
+# log out
+docker logout karcontainers-microsoft.azurecr.io
+
 
--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,5 @@
-FROM node
+﻿FROM node
 MAINTAINER Kevin Remde
-
-#RUN apt-get install -y software-properties-common python
-#RUN add-apt-repository ppa:chris-lea/node.js
-#RUN echo "deb http://us.archive.ubuntu.com/ubuntu/ precise universe" >> /etc/apt/sources.list
-#RUN apt-get update
-#RUN apt-get install -y nodejs
-#RUN apt-get install -y nodejs=0.6.12~dfsg1-1ubuntu1
 
 ENV PORT=80
 EXPOSE 80
